9b97d542d21b8c4c400f5b8161f064ceaa0f34f9
Navita Ramasar <navitaramasar14@gmail.com>
2023-09-28 11:38:02 -0400

    git update

--- travelpage/src/Components/DestinationList.js
+++ travelpage/src/Components/DestinationList.js
@@ -21,18 +21,24 @@ export default function DestionationList({city,image,funfact}){
 
 
     return (
-        <div class="flex flex-col items-center">
-            <div>
-                <h2 class="text-xl font-bold font-serif">{city}</h2>
-            </div>
-            <div>
-            <img src={image} alt={city} />
-                <NavLink 
-                exact 
-                to={`/destination/${city}`}>
-                     <button class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-3 rounded-full bg-blue-500 shadow-lg shadow-blue-500/50">More Info</button>
-                </NavLink>
+    <div class="flex justify-center">
+        <div class="flex flex-wrap justify-center">
+            <div class="max-w-xs rounded overflow-hidden shadow-lg justify-center">
+                <div class="text-center">
+                    <h2 class="text-xl font-bold font-serif">{city}</h2>
+                </div>
+                <div>
+                <img class="w-full" src={image} alt={city} />
+                    <NavLink 
+                    exact 
+                    to={`/destination/${city}`}>
+                        <div class="px-6 py-4 flex items-center justify-center">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-3 rounded-full bg-blue-500 shadow-lg shadow-blue-500/50">More Info</button>
+                        </div>
+                    </NavLink>
+                </div>
             </div>
         </div>
+    </div>
     )
 }
