3804528e4248f6f5692fceccc88a312c64962ed9
Juan Lugo <juanlugodev@gmail.com>
2023-09-28 11:31:29 -0400

    done cards

---
+++ travelpage/src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { AiFillGithub } from 'react-icons/ai'
+
+export default function Footer() {juan
+ return (
+    <div className='bg-cyan-50 h-20 items-center max-w-[1400px] mx-auto pb-1 flex justify-between'>
+
+        <div className='flex flex-row m-10 uppercase'>
+            <div className="p-3"> 
+               <AiFillGithub size={30}/>
+            </div>
+            <h1 className="p-4 font-bold">jotasavage</h1>
+            <h1 className="p-4 font-bold">acabacadabolis</h1>
+            <h1 className="p-4 font-bold">navita14</h1>
+        </div>
+        
+    </div>
+
+
+    )
+}
